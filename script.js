e93ee4a859f6b0ce8d4655dd211bb001e3443bff
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-05-22 14:27:36 -0300

    add drawing functionality

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 let currentColor = "black";
+let canDraw = false;
+let mouseX = 0;
+let mouseY = 0;
 
 let screen = document.querySelector("#screen");
 let ctx = screen.getContext("2d");
@@ -7,10 +10,47 @@ document.querySelectorAll(".colorArea .color").forEach((item) => {
   item.addEventListener("click", colorClickEvent);
 });
 
+screen.addEventListener("mousedown", mouseDownEvent);
+screen.addEventListener("mousemove", mouseMoveEvent);
+screen.addEventListener("mouseup", mouseUpEvent);
+
 function colorClickEvent(e) {
   let color = e.target.getAttribute("data-color");
   currentColor = color;
 
   document.querySelector(".color.active").classList.remove("active");
   e.target.classList.add("active");
+}
+
+function mouseDownEvent(e) {
+  canDraw = true;
+  mouseX = e.pageX - screen.offsetLeft;
+  mouseY = e.pageY - screen.offsetTop;
+}
+
+function mouseMoveEvent(e) {
+  if (canDraw) {
+    draw(e.pageX, e.pageY);
+  }
+}
+
+function mouseUpEvent() {
+  canDraw = false;
+}
+
+function draw(x, y) {
+  let pointX = x - screen.offsetLeft;
+  let pointY = y - screen.offsetTop;
+
+  ctx.beginPath();
+  ctx.lineWidth = 5;
+  ctx.lineJoin = "round";
+  ctx.moveTo(mouseX, mouseY);
+  ctx.lineTo(pointX, pointY);
+  ctx.closePath();
+  ctx.strokeStyle = currentColor;
+  ctx.stroke();
+
+  mouseX = pointX;
+  mouseY = pointY;
 }

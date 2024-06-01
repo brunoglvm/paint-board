91e3ffd0a421bd5799d66c257a05d0dbe08ec7d3
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-06-01 00:13:04 -0300

    add jscolor config

---
+++ jscolor.config.js
@@ -0,0 +1,14 @@
+jscolor.presets.default = {
+  format: "hex",
+  backgroundColor: "rgb(33, 38, 48)",
+  controlBorderColor: "#555",
+  buttonColor: "#222",
+  buttonBorderColor: "#555",
+  width: 200,
+  borderWidth: 0,
+  borderColor: "#222",
+  insetColor: "#222",
+  controlBorderWidth: 1,
+  controlBorderColor: "#444",
+  shadowColor: "#222",
+};

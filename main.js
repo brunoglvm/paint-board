4ae8bd3072e1197abfe570aa094de144f4f6d85c
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-06-06 21:41:20 -0300

    refactor: improve code structure

--- main.js
+++ main.js
@@ -2,93 +2,38 @@ let canDraw = false;
 let mouseX = 0;
 let mouseY = 0;
 let currentColor = "#000000";
-let undoStack = [];
-let redoStack = [];
+const undoStack = [];
+const redoStack = [];
 
 const screen = document.querySelector("#screen");
 const ctx = screen.getContext("2d");
 const modal = document.querySelector("#modal");
 
-document.querySelector("#clear").addEventListener("click", function () {
-  modal.style.display = "flex";
-});
-
-document.querySelector("#confirmClear").addEventListener("click", function () {
-  clearScreen(true);
-  modal.style.display = "none";
-});
-
-document.querySelector("#save").addEventListener("click", async function () {
-  const image = await screen.toDataURL("image/png");
-
-  if (window.showSaveFilePicker) {
-    const options = {
-      suggestedName: "paint-board.png",
-      types: [
-        {
-          description: "PNG file",
-          accept: {
-            "image/png": [".png"],
-          },
-        },
-      ],
-    };
-
-    try {
-      const handle = await window.showSaveFilePicker(options);
-      const writable = await handle.createWritable();
-      await writable.write(await (await fetch(image)).blob());
-      await writable.close();
-    } catch (err) {
-      console.error("An error occurred while saving the file:", err);
-    }
-  } else {
-    const link = document.createElement("a");
-    link.href = image;
-    link.download = "paint-board.png";
-    link.click();
-  }
-});
-
-document.querySelector("#cancelClear").addEventListener("click", function () {
-  modal.style.display = "none";
-});
-
-modal.addEventListener("click", function (e) {
-  if (e.target === modal) {
+function initializeEvents() {
+  document.querySelector("#save").addEventListener("click", saveImage);
+  document.querySelector("#clear").addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+  document.querySelector("#confirmClear").addEventListener("click", () => {
+    clearScreen(true);
     modal.style.display = "none";
-  }
-});
-
-screen.addEventListener("mousedown", mouseDownEvent);
-screen.addEventListener("mousemove", mouseMoveEvent);
-screen.addEventListener("mouseup", mouseUpEvent);
-screen.addEventListener("mouseleave", mouseLeaveEvent);
-
-document.querySelector("#colorPicker").addEventListener("change", function () {
-  let color = this.jscolor.toHEXString();
-  currentColor = color;
-
-  localStorage.setItem("selected-color", currentColor);
-});
-
-document.addEventListener("DOMContentLoaded", function () {
-  const savedColor = localStorage.getItem("selected-color");
-  if (savedColor) {
-    currentColor = savedColor;
-    document.querySelector("#colorPicker").jscolor.fromString(savedColor);
-  }
-
-  loadDrawing();
-});
-
-function colorClickEvent(e) {
-  let color = e.target.getAttribute("data-color");
-  currentColor = color;
-  document.querySelector(".color.active").classList.remove("active");
-  e.target.classList.add("active");
-
-  localStorage.setItem("selected-color", currentColor);
+  });
+  document.querySelector("#cancelClear").addEventListener("click", () => {
+    modal.style.display = "none";
+  });
+  modal.addEventListener("click", (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none";
+    }
+  });
+  screen.addEventListener("mousedown", mouseDownEvent);
+  screen.addEventListener("mousemove", mouseMoveEvent);
+  screen.addEventListener("mouseup", mouseUpEvent);
+  screen.addEventListener("mouseleave", mouseLeaveEvent);
+  document
+    .querySelector("#colorPicker")
+    .addEventListener("change", changeColor);
+  document.addEventListener("DOMContentLoaded", loadInitialSettings);
 }
 
 function mouseDownEvent(e) {
@@ -120,9 +65,15 @@ function mouseLeaveEvent() {
   }
 }
 
+function changeColor() {
+  const color = this.jscolor.toHEXString();
+  currentColor = color;
+  localStorage.setItem("selected-color", currentColor);
+}
+
 function draw(x, y) {
-  let pointX = x - screen.offsetLeft;
-  let pointY = y - screen.offsetTop;
+  const pointX = x - screen.offsetLeft;
+  const pointY = y - screen.offsetTop;
   ctx.beginPath();
   ctx.lineWidth = 5;
   ctx.lineJoin = "round";
@@ -149,3 +100,45 @@ function saveState() {
   undoStack.push(screen.toDataURL());
   redoStack.length = 0;
 }
+
+async function saveImage() {
+  const image = await screen.toDataURL("image/png");
+
+  if (window.showSaveFilePicker) {
+    const options = {
+      suggestedName: "paint-board.png",
+      types: [
+        {
+          description: "PNG file",
+          accept: { "image/png": [".png"] },
+        },
+      ],
+    };
+
+    try {
+      const handle = await window.showSaveFilePicker(options);
+      const writable = await handle.createWritable();
+      await writable.write(await (await fetch(image)).blob());
+      await writable.close();
+    } catch (err) {
+      console.error("An error occurred while saving the file:", err);
+    }
+  } else {
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "paint-board.png";
+    link.click();
+  }
+}
+
+function loadInitialSettings() {
+  const savedColor = localStorage.getItem("selected-color");
+  if (savedColor) {
+    currentColor = savedColor;
+    document.querySelector("#colorPicker").jscolor.fromString(savedColor);
+  }
+
+  loadDrawing();
+}
+
+initializeEvents();
